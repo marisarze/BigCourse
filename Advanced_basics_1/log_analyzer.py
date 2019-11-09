e0e98caebe5dad7a183cac711b5c6e8ae89e959d
marisarze <flypetrov@yandex.ru>
2019-11-09 18:35:05 +0300

    minor changes

--- Advanced_basics_1/log_analyzer.py
+++ Advanced_basics_1/log_analyzer.py
@@ -9,6 +9,7 @@
 
 import sys
 import os
+import json
 
 config = {
     "REPORT_SIZE": 1000,
@@ -16,19 +17,28 @@ config = {
     "LOG_DIR": "./log"
 }
 
+default_config_dir = r'/usr/local/etc/'
 
 def main():
     if '--config' in sys.argv:
         config_dir = sys.argv[sys.argv.index('--config')+1]
     else:
-        config_dir = config['LOG_DIR']
+        config_dir = default_config_dir
     
     try:
-        config_file = open(config_dir, 'r')
+        config_file = open(default_config_dir, 'r')
+        temp_str = config_file.read()
+        temp_str = temp_str.replace("\'", "\"")
+        priority_config = json.loads(temp_str)
     except:
-        print "Could not read file:", config_dir
-        
+        print (":", config_dir)
+
+    if priority_config:
+        for key in priority_config.keys():
+            config[key]=priority_config[key]    
     
+    input_dir = config["LOG_DIR"]
+    file_names = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
 
 
 if __name__ == "__main__":
