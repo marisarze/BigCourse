1250ec3e02d24f0577c5de1032a595a0071be456
Andrey <flypetrov@yandex.ru>
2019-11-11 21:35:48 +0300

    In the process of creating a parser generator

--- Advanced_basics_1/log_analyzer.py
+++ Advanced_basics_1/log_analyzer.py
@@ -12,47 +12,48 @@ import os
 import json
 import re
 import gzip
+import logging
 
+logging.basicConfig(format='[%(asctime)s] %(levelname)s %(message)s', 
+                    datefmt='%Y.%m.%d %H:%M:%S',
+                    level=logging.INFO)
 config = {
     "REPORT_SIZE": 1000,
     "REPORT_DIR": "./reports",
     "LOG_DIR": "./log"
 }
 
+
 default_config_dir = r'C:\Users\Tom\Documents\temp\config.txt'
 
 
-def main():
-    priority_config = {}
-    if '--config' in sys.argv:
-        config_dir = sys.argv[sys.argv.index('--config')+1]
-    else:
-        config_dir = default_config_dir
-    
+def get_new_config(path, old_config):
     try:
-        config_file = open(config_dir, 'r')
+        logging.info('Trying open file: %s' %path)
+        config_file = open(path, 'r')
+        logging.info('Trying read file: %s' %path)
         temp_str = config_file.read()
-        #temp_str = temp_str.replace("\'", "\"")
-        priority_config = json.loads(temp_str)
-        print priority_config
     except:
-        print 'Config file is not exist or corrupted'
+        logging.exception('Error occured when trying open or read the file in %s' %path)
+        sys.exit()
+
+    try:
+        logging.info('Trying retrieve data from config file')
+        priority_config = json.loads(temp_str)
+    except:
+        logging.exception('Error: Config file contains incorrect data')
+        sys.exit()
 
     if priority_config:
         for key in priority_config.keys():
-            config[key]=priority_config[key]    
-    
-    input_dir = config["LOG_DIR"]
-    file_names = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
-    file_names = [f for f in file_names if re.search("^nginx-access-ui.log-[0-9]{8}",f)]
+            old_config[key]=priority_config[key]
+    return old_config
 
-    if file_names:
-        input_file = max(file_names)
-        if input_file.endswith(".gz"):
-            logfile = gzip.open(os.path.join(input_dir, input_file))
-        else:
-            logfile = open(os.path.join(input_dir, input_file))
-
+def parsing_generator(path):
+    if path.endswith(".gz"):
+        input_file = gzip.open(path)
+    else:
+        input_file = open(path)
     template = ' '.join([
         '(?P<remote_addr>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})',  # '$remote_addr                       
         '(?P<remote_user>.+|\-)', #  $remote_user 
@@ -70,8 +71,35 @@ def main():
         '(?P<request_time>(\-)|(.+))' # '$request_time'; 
     ])
     line_format = re.compile(template, re.IGNORECASE)
+    for line in input_file:
+        data = re.search(line_format, line)
+        if data:
+            data = data.groupdict()
 
-    count, count_perc, time_sum, time_perc, time_avg, time_max, time_med = {}, {}, {}, {}, {}, {}, {}
+
+def main():
+    if '--config' in sys.argv:
+        config_dir = sys.argv[sys.argv.index('--config')+1]
+    else:
+        config_dir = default_config_dir
+
+    config = get_new_config(config_dir, config)
+    if 'OUT_LOG' in config.keys():
+        logging.info('Setting logging output to %s' %config['OUT_LOG'])
+        logging.basicConfig(filename=config['OUT_LOG'], 
+                    format='[%(asctime)s] %(levelname)s %(message)s', 
+                    datefmt='%Y.%m.%d %H:%M:%S',
+                    level=logging.INFO)
+
+
+    input_dir = config["LOG_DIR"]
+    file_names = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
+    file_names = [f for f in file_names if re.search("^nginx-access-ui.log-[0-9]{8}\.(gz|plain)",f)]
+    if file_names:
+        file_name = os.path.join(input_dir, max(file_names))
+
+
+    
     time_list = dict()
     all_lines = logfile.readlines()
     bad_count = 0
