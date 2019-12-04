0b7a39ea8fffea9c9313378f5e338b32c9425a18
Andrey <flypetrov@yandex.ru>
2019-12-04 08:52:44 +0300

    oop_3 continued

--- OOP_3/api.py
+++ OOP_3/api.py
@@ -10,6 +10,8 @@ import uuid
 import re
 from optparse import OptionParser
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from scoring import get_score
+from scoring import get_interests
 
 SALT = "Otus"
 ADMIN_LOGIN = "admin"
@@ -56,7 +58,20 @@ class CharField(Field):
 
 
 class ArgumentsField(Field):
-    pass
+    def validate(self, value):
+        if isinstance(value, dict):
+            variants = [['phone', 'email'],
+                        ['name', 'last_name'],
+                        ['gender', 'birthday']]
+            cases = []
+            for pair in variants:
+                case = True
+                for name in pair:
+                    case = case and (name in value.keys())
+                cases.append(case)
+            return any(cases)
+        return False
+
 
 
 class EmailField(Field):
@@ -110,8 +125,7 @@ class ORMMeta(type):
     def __new__(self, class_name, bases, namespace):
         fields = {
             name: field
-            for name, field in namespace.items()
-            if isinstance(field, Field)
+            for name, field in namespace.items() if isinstance(field, Field)
         }
 
         new_namespace = namespace.copy()
@@ -126,11 +140,12 @@ class ORMBase(metaclass=ORMMeta):
 
     def __init__(self, **kwargs):
         _required = []
+        class_name = self.__class__.__name__
         for name in self._fields.keys():
             if self._fields[name].required and (name not in kwargs.keys()):
                 _required.append(name)
         if _required:
-            raise AttributeError('Additional fields are required to create class {}: {} '.format(self.__class__.__name__, ','.join(_required)))
+            raise AttributeError('Additional fields are required to create class {}: {} '.format(self.class_name, ','.join(_required)))
         
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -141,21 +156,18 @@ class ORMBase(metaclass=ORMMeta):
             if self._fields[key].validate(value):
                 super().__setattr__(key, value)
             else:
-                raise AttributeError('Invalid value "{}" for field "{}"'.format(value, key))
+                raise AttributeError('Invalid value "{}" for field {} in class "{}"'.format(value, key, self.class_name))
         else:
-            raise AttributeError('Unknown field "{}"'.format(key))
+            raise AttributeError('Unknown field "{}" in class {}'.format(key, self.class_name))
 
-    def to_json(self):
+    def get_fields(self):
         """ Convert given object to JSON """
         new_dictionary = {}
         allattrs = [name for name in dir(self) if name in self._fields.keys()]
         for name in allattrs:
             new_dictionary[name] = getattr(self, name)
 
-        return json.dumps(new_dictionary)
-
-
-
+        return new_dictionary
 
 
 class ClientsInterestsRequest(ORMBase):
@@ -172,92 +184,103 @@ class OnlineScoreRequest(ORMBase):
     gender = GenderField(required=False, nullable=True)
 
 
-cc = OnlineScoreRequest(first_name='kerr', last_name='male')
-print(cc.__class__.__name__)
-print('example: ', cc.to_json())
-cc.first_name = 
-# class MethodRequest(object):
-#     account = CharField(required=False, nullable=True)
-#     login = CharField(required=True, nullable=True)
-#     token = CharField(required=True, nullable=True)
-#     arguments = ArgumentsField(required=True, nullable=True)
-#     method = CharField(required=True, nullable=False)
+class MethodRequest(ORMBase):
+    account = CharField(required=False, nullable=True)
+    login = CharField(required=True, nullable=True)
+    token = CharField(required=True, nullable=True)
+    arguments = ArgumentsField(required=True, nullable=True)
+    method = CharField(required=True, nullable=False)
 
-#     @property
-#     def is_admin(self):
-#         return self.login == ADMIN_LOGIN
+    
+    @property
+    def is_admin(self):
+        return self.login == ADMIN_LOGIN
 
 
-# def check_auth(request):
-#     if request.is_admin:
-#         digest = hashlib.sha512(datetime.datetime.now().strftime("%Y%m%d%H") + ADMIN_SALT).hexdigest()
-#     else:
-#         digest = hashlib.sha512(request.account + request.login + SALT).hexdigest()
-#     if digest == request.token:
-#         return True
-#     return False
+def check_auth(request):
+    if request.is_admin:
+        digest = hashlib.sha512(datetime.datetime.now().strftime("%Y%m%d%H") + ADMIN_SALT).hexdigest()
+    else:
+        digest = hashlib.sha512(request.account + request.login + SALT).hexdigest()
+    if digest == request.token:
+        return True
+    return False
 
 
-# def method_handler(request, ctx, store):
-#     response, code = None, None
-#     return response, code
+def method_handler(request, ctx, store):
+    response, code = None, None
+    reques_body = request['body']
+    
+    main_request = MethodRequest(request_body)
+    if main_request.method == online_score:
+        auxilary_request = OnlineScoreRequest(main_request.arguments)
+    else:
+        auxilary_request = ClientsInterestsRequest(main_request.arguments)
 
 
-# class MainHTTPHandler(BaseHTTPRequestHandler):
-#     router = {
-#         "method": method_handler
-#     }
-#     store = None
 
-#     def get_request_id(self, headers):
-#         return headers.get('HTTP_X_REQUEST_ID', uuid.uuid4().hex)
-
-#     def do_POST(self):
-#         response, code = {}, OK
-#         context = {"request_id": self.get_request_id(self.headers)}
-#         request = None
-#         try:
-#             data_string = self.rfile.read(int(self.headers['Content-Length']))
-#             request = json.loads(data_string)
-#         except:
-#             code = BAD_REQUEST
-
-#         if request:
-#             path = self.path.strip("/")
-#             logging.info("%s: %s %s" % (self.path, data_string, context["request_id"]))
-#             if path in self.router:
-#                 try:
-#                     response, code = self.router[path]({"body": request, "headers": self.headers}, context, self.store)
-#                 except Exception as e:
-#                     logging.exception("Unexpected error: %s" % e)
-#                     code = INTERNAL_ERROR
-#             else:
-#                 code = NOT_FOUND
-
-#         self.send_response(code)
-#         self.send_header("Content-Type", "application/json")
-#         self.end_headers()
-#         if code not in ERRORS:
-#             r = {"response": response, "code": code}
-#         else:
-#             r = {"error": response or ERRORS.get(code, "Unknown Error"), "code": code}
-#         context.update(r)
-#         logging.info(context)
-#         self.wfile.write(json.dumps(r))
-#         return
+    return response, code
 
 
-# if __name__ == "__main__":
-#     op = OptionParser()
-#     op.add_option("-p", "--port", action="store", type=int, default=8080)
-#     op.add_option("-l", "--log", action="store", default=None)
-#     (opts, args) = op.parse_args()
-#     logging.basicConfig(filename=opts.log, level=logging.INFO,
-#                         format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
-#     server = HTTPServer(("localhost", opts.port), MainHTTPHandler)
-#     logging.info("Starting server at %s" % opts.port)
-#     try:
-#         server.serve_forever()
-#     except KeyboardInterrupt:
-#         pass
-#     server.server_close()
+class MainHTTPHandler(BaseHTTPRequestHandler):
+    router = {
+        "method": method_handler
+    }
+    store = None
+
+    def get_request_id(self, headers):
+        return headers.get('HTTP_X_REQUEST_ID', uuid.uuid4().hex)
+
+    def do_POST(self):
+        print('------------------------------------------------------------------------')
+        response, code = {}, OK
+        context = {"request_id": self.get_request_id(self.headers)}
+        request = None
+        try:
+            print('headers: ', self.headers, end='\n')
+            data_string = self.rfile.read(int(self.headers['Content-Length']))
+            print('data_string: ', data_string.decode(), end='\n')
+            request = json.loads(data_string)
+        except:
+            print('bad_code', end='\n')
+            code = BAD_REQUEST
+
+        if request:
+            path = self.path.strip("/")
+            logging.info("%s: %s %s \n" % (self.path, data_string, context["request_id"]))
+            if path in self.router:
+                try:
+                    response, code = self.router[path]({"body": request, "headers": self.headers}, context, self.store)
+                except Exception as e:
+                    logging.exception("Unexpected error: %s" % e)
+                    code = INTERNAL_ERROR
+            else:
+                code = NOT_FOUND
+
+        self.send_response(code)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+        if code not in ERRORS:
+            r = {"response": response, "code": code}
+        else:
+            r = {"error": response or ERRORS.get(code, "Unknown Error"), "code": code}
+        context.update(r)
+        logging.info(context)
+        self.wfile.write(json.dumps(r).encode())
+        return
+
+
+if __name__ == "__main__":
+    op = OptionParser()
+    op.add_option("-p", "--port", action="store", type=int, default=8080)
+    op.add_option("-l", "--log", action="store", default=None)
+    (opts, args) = op.parse_args()
+    logging.basicConfig(filename=opts.log, level=logging.INFO,
+                        format='[%(asctime)s] %(levelname).1s %(message)s \n', datefmt='%Y.%m.%d %H:%M:%S')
+    server = HTTPServer(("localhost", opts.port), MainHTTPHandler)
+    logging.info("Starting server at %s" % opts.port)
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    server.server_close()
